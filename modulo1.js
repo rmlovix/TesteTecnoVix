5f6a1dc74719e8a60484bc20e90df5bd78c54174
rmlovix <52804120+rmlovix@users.noreply.github.com>
2019-11-05 19:11:15 -0200

    adicionado os exercicios 4 e 5 e finalizado os desafios do modulo 1

--- modulo1.js
+++ modulo1.js
@@ -44,3 +44,38 @@ console.log(temHabilidade(skills))
 
 //exercicio 4
 
+var anosDeEstudo = age => {
+    switch (true) {
+      case age >= 0 && age <= 1:
+        console.log(`Você tem ${age} anos de experiência, seu nível é: Iniciante`);
+        break;
+      case age >= 1 && age <= 3:
+        console.log(`Você tem ${age} anos de experiência, seu nível é: Intermediário`);
+        break;
+      case age >= 3 && age <= 6:
+        console.log(`Você tem ${age} anos de experiência, seu nível é: Avançado`);
+        break;
+      default:
+        console.log(`Você tem ${age} anos de experiência, seu nível é: Jedi Master `);
+        break;
+    }
+  };
+  anosDeEstudo(5);
+
+// De 0-1 ano: Iniciante
+// De 1-3 anos: Intermediário
+// De 3-6 anos: Avançado
+// De 7 acima: Jedi Master
+
+
+//exercicio 5
+
+var usuário = {
+  Diego: "Javascript, ReactJS, Redux",
+  Gabriel: "VueJS, Ruby on Rails, Elixir"
+}
+function habilidades(hab){
+  return `O Diego possui as habilidades: ${hab["Diego"]}
+O Gabriel possui as habilidades: ${hab["Gabriel"]}`
+}
+console.log(habilidades(usuário))
